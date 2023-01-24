8372ce4e1c64788befb84b002622f1bf30dbd19b
MartinSpendl <martin.spendl@gmail.com>
2023-01-24 10:15:26 +0100

    Scipt for downloading data, folder and gitignore

---
+++ download_data.py
@@ -0,0 +1,78 @@
+"""
+Script for downloading data from:
+https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE62944
+
+and storing relevant files into data/ folder.
+"""
+import os
+import urllib.request
+
+EXPRESSION_DATA_NAME = "GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_TPM.txt.gz"
+CLINICAL_DATA_NAME = (
+    "GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz"
+)
+CANCER_DATA_NAME = "GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz"
+
+CLINICAL_DOWNLOAD_LINK = "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE62944&format=file&file=GSE62944%5F06%5F01%5F15%5FTCGA%5F24%5F548%5FClinical%5FVariables%5F9264%5FSamples%2Etxt%2Egz"
+CANCER_DOWNLOAD_LINK = "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE62944&format=file&file=GSE62944%5F06%5F01%5F15%5FTCGA%5F24%5FCancerType%5FSamples%2Etxt%2Egz"
+
+
+def download_expression_data() -> None:
+    """
+    PLEASE download the expression file manually.
+    """
+
+    if EXPRESSION_DATA_NAME in os.listdir("data"):
+        print("Expression data already downloaded.")
+
+    else:
+        print(
+            f"""
+Expression data not found.
+Please follow the instruction to download the expression data manually.
+
+1.) Visit: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE62944
+2.) At the bottom of the page find: GSE62944_RAW.tar
+3.) Press (custom) to expand the selection.
+4.) Toggle the sixth file named: {EXPRESSION_DATA_NAME}
+5.) Press Download button to download.
+6.) Wait a few minutes for GSE62944_RAW.tar folder to download.
+7.) Extract the downloaded folder into the data/ folder.
+8.) Keep the extracted file zipped (.gz) to save space.
+"""
+        )
+
+
+def download_clinical_data() -> None:
+    """
+    Downloads TCGA clinical data from:
+    https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE62944&format=file&file=GSE62944%5F06%5F01%5F15%5FTCGA%5F24%5F548%5FClinical%5FVariables%5F9264%5FSamples%2Etxt%2Egz
+    """
+
+    if CLINICAL_DATA_NAME in os.listdir("data"):
+        print("Clinical data already downloaded.")
+
+    else:
+        urllib.request.urlretrieve(CLINICAL_DOWNLOAD_LINK, f"data/{CLINICAL_DATA_NAME}")
+
+
+def download_cancer_data() -> None:
+    """
+    Downloads TCGA cancer information from:
+    https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE62944&format=file&file=GSE62944%5F06%5F01%5F15%5FTCGA%5F24%5F548%5FClinical%5FVariables%5F9264%5FSamples%2Etxt%2Egz
+    """
+
+    if CANCER_DATA_NAME in os.listdir("data"):
+        print("Cancer data already downloaded.")
+
+    else:
+        urllib.request.urlretrieve(CANCER_DOWNLOAD_LINK, f"data/{CANCER_DATA_NAME}")
+
+
+if __name__ == "__main__":
+
+    download_expression_data()
+
+    download_clinical_data()
+
+    download_cancer_data()
